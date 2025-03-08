84ac2e3af88cf8004de4dde799e098d942da2d99
mingon <mingon0125@gmail.com>
2025-03-09 03:46:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,6 @@
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        long a = 0;
-        
         long sqrt = (long) Math.sqrt(n);
-        if(n == sqrt * sqrt) a = sqrt;
-        else return -1;
-        answer = (a+1) * (a+1);
-        		
-        return answer;
+        return n==Math.pow(sqrt,2) ? (long)Math.pow(sqrt+1, 2) : -1;
     }
 }
