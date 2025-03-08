363a5c81589191f0b1bffc3b5fe90ea850119f00
mingon <mingon0125@gmail.com>
2025-03-08 21:49:54 +0900

    [level 1] Title: 2016년, Time: 1.44 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,8 +2,6 @@ import java.time.LocalDate;
 
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
-        LocalDate date = LocalDate.of(2016, a, b);
-        return date.getDayOfWeek().toString().substring(0,3).toUpperCase();
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
     }
 }
