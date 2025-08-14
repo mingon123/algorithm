a7dfcaaf44d4e497d20dcdf41297c649d66de048
mingon123 <mingon0125@gmail.com>
2025-08-14 23:55:15 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.28 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        my_string = my_string.replaceAll("[A-Za-z]","");
+        for(char c : my_string.toCharArray()){
+            answer += c - '0';
+        }
+        
+        return answer;
+    }
+}
