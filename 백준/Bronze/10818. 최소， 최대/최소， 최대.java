5bf4494ca92edc782032c4013edfe3b17b53d3f5
mingon123 <mingon0125@gmail.com>
2025-11-10 17:38:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 1540 ms, Memory: 312180 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++){
+            int num = sc.nextInt();
+            if(num > max) max = num;
+            if(num < min) min = num;
+        }
+        
+        System.out.println(min + " " + max);
+    }
+}
